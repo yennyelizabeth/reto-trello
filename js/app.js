6411ded2cb7587cec1af9a5446c8615438c4d55b
yennyelizabeth <yenny.elizabeth@hotmail.com>
2017-11-22 14:35:14 -0500

    versión-2

--- js/app.js
+++ js/app.js
@@ -12,8 +12,33 @@ function begin() {
   addList.addEventListener('click', createList); /* evento click -> llama función createList */
 };
 
-/* función createList que  */
-function createList(event) {
+/* función newList que agrega o quita la clase de visibilidad */
+function newListt(event) {
   event.target.classList.toggle('display-none'); /* con toggle agrega la clase que no tenía - oculta span */
   form.classList.toggle('display-none'); /* con toggle quita la clase que ya tenia form inicialmente - aparece form */
 };
+
+/* función createList visualiza lista */
+function createList(event) {
+  event.preventDefault();
+
+  if(nameList.value) { /* si nameList no es vacío "true" */
+    form.classList.toggle('display-none'); /* desaparece form */
+
+    var title = nameList.value.toUpperCase(); /* declara,asigna y convierte mayusculas */
+    var titleElement = document.createElement('div'); /* agrega clase */
+
+    titleElement.textContent = title; /* agrega clase */
+
+    form.parentElement.insertBefore(titleElement, form); /* agrega hijo a padre de form, antes de su hermano form*/
+
+    var link = document.createElement("a"); /* declara link , asigna la creacion elemento a */
+    link.setAttribute("href", "#"); /* agrega el atributo href con valor # */
+
+    var linkText = document.createTextNode("Añadir una tarea..."); /* declara linkText, asigna la creación de nodo texto con contenido */
+    link.appendChild(linkText); /* asigna un hijo linkText al padre link */
+
+    titleElement.parentElement.appendChild(link); /* asigna hijo link a padre titleElement */
+  }
+}
+
